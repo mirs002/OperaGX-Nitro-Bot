295a28097b7be82c7c1b90dae34ce61a3ae9c61b
mirs002 <91021014+mirs002@users.noreply.github.com>
2023-12-21 23:59:20 +0300

    Update main.py
    improved version with the help of KiSki-Dev

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import requests
 import uuid
 import time
-
-loop_amount = input("How many codes do you want?: ")
-interval = input("How long do you want to wait between each code (in seconds)?: ")
 url = "https://api.discord.gx.games/v1/direct-fulfillment"
 headers = {
             "Content-Type": "application/json",
@@ -11,21 +8,28 @@ headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0",
         }
 data = {"partnerUserId": str(uuid.uuid4())}
-
-if loop_amount != "inf": 
+        
+def generation(loop_amount, interval):              
     for i in range(int(loop_amount)):
-            
-        response = requests.post(url, json=data, headers=headers)
-        promo = f"https://discord.com/billing/partner-promotions/1180231712274387115/{response.json().get('token')}"
-        with open("promotions.txt", "a") as f:
-                                f.write(f"\n{promo}\n")
-        time.sleep(float(interval))
-elif loop_amount == "inf":
-    while True:
-        response = requests.post(url, json=data, headers=headers)
-        promo = f"https://discord.com/billing/partner-promotions/1180231712274387115/{response.json().get('token')}"
-        with open("promotions.txt", "a") as f:
-                                f.write(f"\n{promo}\n")
-        time.sleep(float(interval))
-else:
-    print("Invalid input.")
+        try:
+            response = requests.post(url, json=data, headers=headers)
+            promo = f"https://discord.com/billing/partner-promotions/1180231712274387115/{response.json().get('token')}"
+            with open("promotions.txt", "a") as f:f.write(f"\n{promo}\n")
+            print("Generated new code: " + promo)
+            print("Total Codes: " + str(i+1) + "\n")
+            time.sleep(float(interval))
+        except:
+            print("An Error happened. Please wait a moment, if this issue persists, please restart the program.")
+            time.sleep(15)
+            continue
+
+if __name__ == "__main__":
+    loop_amount = input('How many Codes do you want?\n"Inf" for infinite.\nAmount: ')
+    print("")
+    interval = input('How long do you want to wait between each generation?\nDont make it too short, or you will get timed out quickly.\nSeconds (Press "Enter" for default): ')
+    print("")
+    if interval == "":
+        interval = 1
+    if loop_amount.lower() == "inf":
+        loop_amount = 999999999999
+    generation(loop_amount, interval)
